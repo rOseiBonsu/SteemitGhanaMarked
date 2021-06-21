73acdc702beba02d1bc8e59ff9010be7186f5fc3
Richmond Osei Bonsu <69705553+rOseiBonsu@users.noreply.github.com>
2021-06-21 20:36:29 +0000

    Add files via upload

---
+++ Steemit markdown/index.js
@@ -0,0 +1,13 @@
+
+
+
+const togglebutton = document.querySelector(".toggle-button");
+const navLinks = document.querySelector(".nav-bar ul");
+
+togglebutton.addEventListener('click',()=>{
+    navLinks.classList.toggle("active"); 
+})
+
+
+
+
---
+++ Steemit markdown/index2.js
@@ -0,0 +1,12 @@
+const togglebuttonz = document.querySelector(" .nav-bar .toggle-button");
+const navLinkz = document.querySelector(".nav-bar ul");
+
+togglebuttonz.addEventListener('click',()=> {
+    navLinkz.classList.toggle("active") 
+});
+
+
+
+
+
+
